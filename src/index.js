842adaf5d9b0734038130803fcaa88d109336c66
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-07-26 00:52:39 +0300

    hero

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './sass/main.scss';
 // import './js/script';
 import './js/reviews';
 import './js/backdrop';
-import './js/video';
 import './js/slick';
-import './js/gallery'
+import './js/gallery';
 import './js/faq';

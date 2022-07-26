ca3534b5c5290ac7411fd30a735b6c8c10cbdc64
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-07-26 20:07:56 +0300

    add partial cases

--- src/js/header.js
+++ src/js/header.js
@@ -17,31 +17,29 @@
     }
   });
 
+  function toggleClassHeader(counterScrollMove, pixels) {
+    if (counterScrollMove > pixels) {
+      refs.header.classList.add('opacity');
+    } else {
+      refs.header.classList.remove('opacity');
+    }
+    window.removeEventListener('resize', handleResize);
+    window.removeEventListener('scroll', handleResize);
+  }
+
   function handleResize() {
     const { innerWidth: width } = window;
     if (width < 768) {
-      return window.addEventListener('scroll', e => {
+      return window.addEventListener('scroll', () => {
         let counterScrollMove = pageYOffset;
         let pixels = 140;
-        if (counterScrollMove > pixels) {
-          refs.header.classList.add('opacity');
-        } else {
-          refs.header.classList.remove('opacity');
-        }
-        window.removeEventListener('resize', handleResize);
-        window.removeEventListener('scroll', handleResize);
+        toggleClassHeader(counterScrollMove, pixels);
       });
     } else {
-      return window.addEventListener('scroll', e => {
+      return window.addEventListener('scroll', () => {
         let counterScrollMove = pageYOffset;
         let pixels = 90;
-        if (counterScrollMove > pixels) {
-          refs.header.classList.add('opacity');
-        } else {
-          refs.header.classList.remove('opacity');
-        }
-        window.removeEventListener('resize', handleResize);
-        window.removeEventListener('scroll', handleResize);
+        toggleClassHeader(counterScrollMove, pixels);
       });
     }
   }

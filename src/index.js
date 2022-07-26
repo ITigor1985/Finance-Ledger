eb75df5d750695c3236257a17e44953683c52033
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-07-27 01:36:59 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import 'material-icons/iconfont/filled.css';
+import 'lightbox2/dist/css/lightbox';
 import './sass/main.scss';
 
 // import './js/script';
+import 'jquery/dist/jquery';
 import './js/header';
+import 'lightbox2/dist/js/lightbox';
 import './js/reviews';
 import './js/backdrop';
 import './js/slick';

922ded127adfa69c6a8268e63b59e8dd341b75fb
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-07-27 21:16:25 +0300

    deleted

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import 'material-icons/iconfont/filled.css';
 import 'lightbox2/dist/css/lightbox';
 import './sass/main.scss';
 
-// import './js/script';
 import 'jquery/dist/jquery';
 import './js/header';
 import 'lightbox2/dist/js/lightbox';
 import './js/contacts';
-import './js/reviews';
-import './js/backdrop';
-import './js/slick';
-import './js/gallery';
-import './js/faq';

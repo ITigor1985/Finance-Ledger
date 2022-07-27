002527cbb2b0a4e8537b510aebbc4c9dc6977bbf
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-07-28 02:46:55 +0300

    form compl

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import 'lightbox2/dist/css/lightbox';
 import './sass/main.scss';
 
 import 'jquery/dist/jquery';
-import './js/header';
 import 'lightbox2/dist/js/lightbox';
+import './js/smoothScroll';
+import './js/header';
 import './js/contacts';
 import './js/form';
